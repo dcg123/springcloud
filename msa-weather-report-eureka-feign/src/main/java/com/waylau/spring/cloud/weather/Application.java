a5b281fb70c3ff99fad741696a1d39dba2a5f0f9
dcg <928532612@qq.com>
2018-09-09 00:38:30 +0800

    更改版本冲突

--- msa-weather-report-eureka-feign/src/main/java/com/waylau/spring/cloud/weather/Application.java
+++ msa-weather-report-eureka-feign/src/main/java/com/waylau/spring/cloud/weather/Application.java
@@ -3,7 +3,9 @@ package com.waylau.spring.cloud.weather;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
